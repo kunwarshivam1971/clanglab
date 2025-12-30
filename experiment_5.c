75051218ec1b2110a43bd8f7914e59da6cc208da
Shivam Kumar Kunwar <1971kunwarshivam@gmail.com>
2025-12-30 23:42:51 +0530

    Update experiment_5.c

--- experiment_5.c
+++ experiment_5.c
@@ -1,1 +1,20 @@
 
+#include <stdio.h>
+
+int main() {
+    int i = 25;
+    float f = 12.34;
+    double d = 123.456789;
+    char c = 'A';
+    char str[] = "Shivam";
+
+    printf("Integer (int): %d\n", i);
+    printf("Float (float): %f\n", f);
+    printf("Double (double): %lf\n", d);
+    printf("Character (char): %c\n", c);
+    printf("String (char array): %s\n", str);
+    printf("Float with 2 decimal places: %.2f\n", f);
+    printf("Double with 3 decimal places: %.3lf\n", d);
+
+    return 0;
+}
