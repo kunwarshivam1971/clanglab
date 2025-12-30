eeadc2fd1799aaa74dd64ba3133c4021adc9629b
Shivam Kumar Kunwar <1971kunwarshivam@gmail.com>
2025-12-30 23:54:34 +0530

    Update experiment_18.c

--- experiment_18.c
+++ experiment_18.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
 
+int main() {
+    int rows, cols, sum = 0;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    int arr[rows][cols];
+
+    printf("Enter elements of the matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            sum += arr[i][j];
+        }
+    }
+
+    printf("Sum of all elements = %d\n", sum);
+
+    return 0;
+}
