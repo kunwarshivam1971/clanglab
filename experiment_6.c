4b4cf3630658648843bf6fc77821ad3fa124ce37
Shivam Kumar Kunwar <1971kunwarshivam@gmail.com>
2025-12-30 23:43:26 +0530

    Update experiment_6.c

--- experiment_6.c
+++ experiment_6.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+void swap(int *x, int *y) {
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int main() {
+    int a, b;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    printf("Before swap: a = %d, b = %d\n", a, b);
+
+    swap(&a, &b);
+
+    printf("After swap: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
